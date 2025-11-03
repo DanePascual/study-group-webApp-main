c723e7a7439f282cbdc6336d46b825c7c4a0b6ba
Dane <dcpascual@paterostechnologicalcollege.edu.ph>
2025-11-03 11:54:39 +0800

    Fix: Frontend feature

--- frontend/config/appConfig.js
+++ frontend/config/appConfig.js
@@ -6,7 +6,7 @@ const DEFAULT_API_BASE =
   "https://study-group-backend-d8fc93ae1b7a.herokuapp.com";
 
 // For local development (comment out to use production):
-// const DEFAULT_API_BASE = "http://localhost:5000";
+//const DEFAULT_API_BASE = "http://localhost:5000";
 
 export const API_BASE =
   (typeof window !== "undefined" && window.API_BASE) || DEFAULT_API_BASE;
